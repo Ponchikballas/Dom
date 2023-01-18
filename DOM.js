f717fca667e5073f3c0819563c5c08e06f67f8a3
Boris Prokuror <122551971+Ponchikballas@users.noreply.github.com>
2023-01-18 10:04:14 +0300

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,81 @@
+// // 241-1
+// let p1 = document.querySelector('#elem1');
+// let p2 = document.querySelector('#elem2');
+// let p3 = document.querySelector('#elem3');
+// console.log(p1);
+// console.log(p2);
+// console.log(p3);
+
+// // 241-2
+// let elem = document.querySelector('#block p')
+// console.log(elem)
+
+
+// // 241-3
+// let elem = document.querySelector('.block p')
+// console.log(elem)
+
+
+// // 241-4
+// let elem= document.querySelector('.www')
+// console.log(elem);
+
+
+// // 242-1
+// let button1 = document.querySelector('#button1');
+// button1.addEventListener('click', function(){
+//     console.log('1');
+// });
+
+// let button2 = document.querySelector('#button2');
+// button2.addEventListener('click', function(){
+//     console.log('2');
+// });
+// let button3 = document.querySelector('#button3');
+// button3.addEventListener('click', function(){
+//     console.log('3');
+// });
+
+
+
+
+// // 243-1
+// let button1 = document.querySelector('#button1');
+// button1.addEventListener('click', func1);
+// function func1() {
+// 	console.log(1);
+// }
+// let button2 = document.querySelector('#button2');
+// button2.addEventListener('click', func2);
+// function func2() {
+// 	console.log(2);
+// }
+
+
+// // 244-1
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// let elem3 = document.querySelector('#elem3');
+// let elem4 = document.querySelector('#elem4');
+// let elem5 = document.querySelector('#elem5');
+
+// elem1.addEventListener('click', func);
+// function func() {
+// 	console.log('message');
+// }
+// elem2.addEventListener('click', func);
+// function func() {
+// 	console.log('message');
+// }
+// elem3.addEventListener('click', func);
+// function func() {
+// 	console.log('message');
+// }
+// elem4.addEventListener('click', func);
+// function func() {
+// 	console.log('message');
+// }
+// elem5.addEventListener('click', func);
+// function func() {
+// 	console.log('message');
+// }
